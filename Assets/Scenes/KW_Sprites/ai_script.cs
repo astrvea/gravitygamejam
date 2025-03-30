852e661cbb69b4ea874357627f8f56e3ec8cdc59
alexvas12 <alexva1374@gmail.com>
2025-03-30 15:26:55 -0400

    script edits

--- Assets/Scenes/KW_Sprites/ai_script.cs
+++ Assets/Scenes/KW_Sprites/ai_script.cs
@@ -3,12 +3,6 @@ using UnityEngine;
 
 public class ai_script : MonoBehaviour
 {
-
-    public Transform target_to_move;
-
-    public float large_circle_radius = 0.5f;
-    public float move_time = 10.0f;
-
     Vector3 target;
     float timer = 3;
     private void Start()
