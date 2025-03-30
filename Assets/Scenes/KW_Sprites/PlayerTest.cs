5a9221c26bd814fdec484a397a53ca68b6fb8260
Octopengi <146447280+Octopengi@users.noreply.github.com>
2025-03-30 12:04:34 -0400

    everything fixed (crab rave)
    ty chappell roan

--- Assets/Scenes/KW_Sprites/PlayerTest.cs
+++ Assets/Scenes/KW_Sprites/PlayerTest.cs
@@ -18,8 +18,9 @@ public class PlayerTest : MonoBehaviour
     float dashCD = 0;
     String direct = "Right";
     Vector2 respawn = new Vector2(0.0f, 0.0f);
-    Vector2 moveVec;
     int cpNum = 0;
+
+    public float spdMLT;
 
     float cTime = 0; //Coyote time
     int doubleJump = 1;
@@ -27,7 +28,7 @@ public class PlayerTest : MonoBehaviour
 
     LayerMask groundMask;
     Transform feet;
-    Vector2 feetBox = new Vector2(0.5f, 0.15f);
+    Vector2 feetBox = new Vector2(0.5f, 0.35f);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -39,58 +40,62 @@ public class PlayerTest : MonoBehaviour
     // Update is called once per frame
     void Update() {
         move = Input.GetAxis("Horizontal");
+        //Is the player standing on something?
         grounded = Physics2D.OverlapBox(feet.position, feetBox, 0, groundMask);
         
+        //Dash stuff, ignore unless we want the dash later.
         if (dashCD > 0){dashCD -= Time.deltaTime;}
         if (dashCD >= 0.25){move = 0; rb.linearVelocityY = 0;}
         if (dashCD < 0.25 && dash == -1){dash = 0; rb.linearVelocityX = 0;}
         if (dashOrb && dash == 0) {dash = 1; dashOrb = false;}
 
+        //Coyote frames; The player keeps the ability to ground jump for a few frames after walking off a ledge
         if (grounded) {
             cTime = 0.2f; doubleJump = 1; dash = 1; //rb.gravityScale = 1.0f;
         } else if (cTime > 0) {cTime -= Time.deltaTime;}
 
+        //Jump
         if (Input.GetButtonDown("Jump")  && dashCD < 0.5f)
             if (grounded) {jump = true;}
             else if (cTime > 0 && rb.linearVelocityY < 0) {jump = true;}
             else if (doubleJump == 1) {jump = true; doubleJump = 0;} 
 
-        if (Input.GetKeyDown(KeyCode.Space) && dash >= 1 && dashCD <= 0){
+        /*if (Input.GetKeyDown(KeyCode.Space) && dash >= 1 && dashCD <= 0){
             dashInp = true;
-        }
+        }*/
 
+        //Rotates the pplayer's feet towards the black hole
         var dir = blackHole.transform.position - transform.position;
         dir.Normalize();
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
 
+        //Test death
         //if (Input.GetKeyDown(KeyCode.F)) {Died();}
     }
 
     void FixedUpdate(){
-        if (Mathf.Abs(rb.linearVelocityY) < 1.0f) {
+        /*if (Mathf.Abs(rb.linearVelocityY) < 1.0f) {
             rb.gravityScale=0.75f; // hang time
         } else if (rb.linearVelocityY < 0.5f) {
             rb.gravityScale = 2.0f; // extra falling gravity
-        }
+        }*/
 
         //limit down and up velocity
-        rb.linearVelocityY = Mathf.Clamp(rb.linearVelocityY, -12, 8);
+        //rb.linearVelocityY = Mathf.Clamp(rb.linearVelocityY, -12, 8);
 
+        //Left + Right movement
         if (move != 0){
-            //rb.linearVelocityX = move * 5;
-            //if (rb.linearVelocityX >= 0){direct = "Right"; sr.sprite = Right;}
-            //else {direct = "Left"; sr.sprite = Left;}
-            rb.linearVelocity = transform.right * move * 5;
+            rb.AddForce(transform.right * move * spdMLT);
         }
 
+        //Jump
         if (jump){
             jump = false;
-            rb.linearVelocityY = 0;
-            rb.gravityScale = 1.0f;
             rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
         }
 
+        //More dash stuff: Ignore unless we want jumping later.
         if (dashInp) {
             dashInp = false; dashCD = .5f; rb.linearVelocityX = 0.0f; rb.linearVelocityY = 0.0f; dash = -1;
             if (direct == "Right") {rb.AddForce(transform.right * dashForce, ForceMode2D.Impulse);}
